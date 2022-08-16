758270c8c6de5b30915e9535e3d5bb9ef83e12ef
Marco Antonio Niño Calderón <55109619+MarcoANT9@users.noreply.github.com>
2022-08-16 16:29:02 -0500

    Added Task 2
    Added __pycache__ to gitignore

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" This module is used to calculate the shape of any given matrix """ 
+
+def matrix_shape(matrix):
+    """
+    This Function calculates the shape of a matrix
+    Arguments:
+        matrix: Matrix to calculate shape
+    return -> Shape of matrix ([int,])
+    """
+    #if not matrix:
+        #return([0])
+    if (len(matrix) == 0):
+        return([])
+    shape = [len(matrix)]
+    vector = matrix[0]
+    while type(vector[0]) != int:
+        shape.append(len(vector))
+        vector = vector[0]
+    shape.append(len(vector))
+    return(shape)
+
+if __name__ == "__main__":
+    main()
